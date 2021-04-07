6aa05220965453547b099d10ff64e018fed50c05
kang-hyun <gusrn8959@naver.com>
2021-04-07 19:57:47 +0900

    disqus update

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+#from django.conf.urls.static import static
+#from django.conf import settings
 
 
 urlpatterns = [
@@ -26,4 +26,4 @@ urlpatterns = [
     path('', include('photo.urls')),
     path('accounts/', include('accounts.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
